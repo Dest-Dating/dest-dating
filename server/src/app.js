c228dc082e613de08d8bc1a73bc8ce4c4b57a1ad
JaTin <jigyashusaini7@gmail.com>
2024-04-05 16:57:15 +0530

    Adding startup functionality and db connections

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,35 @@
 const express = require("express");
-const app = express();
 const morgan = require("morgan"); //a middleware that logs requests onto the console
+const app = express();
 const helmet = require("helmet"); //adds additional HTTP headers
 const mongoSanitize = require("express-mongo-sanitize"); //sanitize the mongo input
 const xss = require("xss-clean"); //removes malicious code from input
 const cors = require("cors"); //prevents cors blockage
 const cookieParser = require("cookie-parser");
 
+//CORS enabled
+// app.all("/", function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     next();
+// });
+
+// security HTTP headers
 app.use(helmet());
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN, credentials: true,
-}));
+//use this if below doesn't work
+// app.use(cors({
+//     origin: ["http://localhost:5173"],
+//     credentials: true,
+// }));
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+
 
 // read data from the body into req.body, max is 10kb.
 app.use(express.json({limit: "10kb"})); //data from body shall be added to req
@@ -36,13 +54,15 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 app.get("/", (req, res, next) => {
     res.status(200).json({
-        status: "success", message: "Welcome to Dest server!",
+        status: "success",
+        message: "Welcome to Duck server!",
     });
 });
 
 app.get("/test", async (req, res, next) => {
     res.status(200).json({
-        status: "success", message: "this is for testing functions",
+        status: "success",
+        message: "this is for testing functions",
     })
 });
 
@@ -50,34 +70,25 @@ app.get("/test", async (req, res, next) => {
 // configuration to parse JSON and URL-encoded data.
 app.use(express.json());
 app.use(express.urlencoded());
-
-
-// Connect to the database
-const db = require("./config/db");
-db.connect()
-    .then(() => {
-        app.on("error", (error) => {
-            console.log("ERROR occurred");
-        });
-        app.listen(process.env.PORT || 8000, () => {
-            console.log(`Server is running at PORT: ${process.env.PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.log(`MONGO DB connection FAILED ${error}`);
-    });
-
 require("./config/cloudinary").cloudinaryConnect();
+
+
+//defining routers
+// todo: routes here
+// const userRouter = require("./routes/userRouters");
+// app.use("/user", userRouter);
 
 
 //for undefined routs
 const AppError = require("./utils/appError");
 app.all("*", (req, res, next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on Dest server!`, 404));
+    next(
+        new AppError(`Can't find ${req.originalUrl} on Duck server!`, 404)
+    );
 });
 
 //in case of operational error this middleware function will be called to return relevant error message
 const globalErrorController = require("./controllers/errorController");
 app.use(globalErrorController);
 
-exports.app = app;
+module.exports = app;
